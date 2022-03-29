f3b2a686a0876005e8645e8e2c3bf25b0ae4eeaf
Akshay Ravindran <qdotdash@gmail.com>
2022-03-29 19:07:31 +0530

    Add speed change

--- game.js
+++ game.js
@@ -1,9 +1,8 @@
 import {
-  SNAKE_SPEED,
   update as updateSnake,
   draw as drawSnake,
   getSnakeHead,
-  snakeIntersection
+  snakeIntersection,
 } from "./snake.js";
 
 import { update as updateFood, draw as drawFood } from "./food.js";
@@ -11,18 +10,27 @@ import { outsideGrid } from "./grid.js";
 
 let lastRender = 0;
 let gameOver = false;
+let RENDER_SPEED = 5;
 const gameBoard = document.getElementById("game-board");
+
+document.getElementById("speed-up").onclick = function () {
+  speedUp();
+};
+document.getElementById("speed-down").onclick = function () {
+  speedDown();
+};
 
 function main(currentTime) {
   if (gameOver) {
-    if(confirm("You Lost, Press ok to restart")){
-        location.replace("https://qdotdash.github.io/snake-game/");
+    if (confirm("You Lost, Press ok to restart")) {
+      location.replace("https://qdotdash.github.io/snake-game/");
     }
-    return
+    return;
   }
   window.requestAnimationFrame(main);
   const timeBetweenRender = (currentTime - lastRender) / 1000;
-  if (timeBetweenRender < 1 / SNAKE_SPEED) return;
+  if (timeBetweenRender < 1 / RENDER_SPEED) return;
+  console.log(RENDER_SPEED);
 
   lastRender = currentTime;
   console.log(timeBetweenRender);
@@ -44,6 +52,16 @@ function draw() {
 
 function gameOverCheck() {
   gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+}
+
+function speedUp() {
+  if (RENDER_SPEED == 100) return;
+  RENDER_SPEED += 1;
+}
+
+function speedDown() {
+  if (RENDER_SPEED == 1) return;
+  RENDER_SPEED -= 1;
 }
 
 window.requestAnimationFrame(main);
